498cadb0fce7c348e3eebdb49adbb072528d56be
Priyanshi Bothra <priyanshibothra753@gmail.com>
2026-04-08 21:17:50 +0530

    fix: correct next config

--- wall-calendar/next.config.ts
+++ wall-calendar/next.config.ts
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: false, // 🔥 important fix
+  },
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'images.unsplash.com',
-    },
-  ],
-}
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
