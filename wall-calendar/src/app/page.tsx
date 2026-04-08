4c7330b96c13f86c42f9fd9f93de80f593081a32
Priyanshi Bothra <priyanshibothra753@gmail.com>
2026-04-08 18:30:59 +0530

    made changes to page.tsx

--- wall-calendar/src/app/page.tsx
+++ wall-calendar/src/app/page.tsx
@@ -1,11 +1,5 @@
-'use client'
+import WallCalendar from '@/components/calendar/WallCalendar';
 
-import WallCalendar from '@/components/calendar/WallCalendar'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-start sm:justify-center px-4 py-8 sm:py-12 bg-gradient-to-br from-gray-100 via-gray-50 to-blue-50 dark:from-gray-950 dark:via-gray-900 dark:to-gray-950">
-      <WallCalendar />
-    </main>
-  )
+export default function Page() {
+  return <WallCalendar />;
 }
